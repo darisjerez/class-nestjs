b693fd0c87d5485cbb366536ec1adc39e71eb752
darisjerez <darisjerez@gmail.com>
2020-08-09 20:55:32 -0400

    add readme and process.env

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as rateLimit from 'express-rate-limit';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-  "origin": "*",
+  "origin": "http://127.0.0.1:5500/",
   "methods": "GET,HEAD,POST,DELETE",
   "preflightContinue": false,
   "optionsSuccessStatus": 204
@@ -16,6 +16,6 @@ async function bootstrap() {
       max: 100, // limit each IP to 100 requests per windowMs
     }),
   );
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
